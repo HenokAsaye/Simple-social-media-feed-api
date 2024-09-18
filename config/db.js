47cb5e5a23b59302bc60e7779873b5b3338231fc
Henok Asaye <henokasaye77@gmail.com>
2024-09-18 17:07:19 +0300

    configure the database

---
+++ config/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectToDb = async () =>{
+    try{
+    await mongoose.connect(process.env.MONGODN_URL)
+    console.log("connected to mongoDb")
+    }catch(error){
+        console.log("Failed to connect to db",error)
+        process.exit(1)
+    }
+
+};
+
+export default connectToDb;
+
