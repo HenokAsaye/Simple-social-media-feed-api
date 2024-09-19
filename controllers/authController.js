0a3632e26eee584f11f7d6501a96eccd5353741b
Henok Asaye <henokasaye77@gmail.com>
2024-09-19 13:17:17 +0300

    add upload folder for profile picture

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,24 @@
 import bcrypt from "bcrypt";
 import { generateToken } from "../utils/jwt.js";
+import User from "../models/userModel.js";
+
+export const signup = async(req,res)=>{
+    const {username,email,password} = req.body;
+    try{
+        const existingUser = await User.find((p)=>p.email === email)
+        if(existingUser){
+            return res.status(409).json({message:`user with email${email} already Exist!`});
+        }
+        const hashedPassword = bcrypt.hash(password,10)
+        const newUser ={
+            username:username,
+            email:email,
+            password:hashedPassword
+        }
+        res.status(201).send("Account Created Successfully!")
+
+    }catch(error){
+        res.status(500).Json({message:"server Error"})
+    }
+}
 
